9095cf3df918b214c21f8178c22187f91938061c
Brian Ujiie <bujiie+github@gmail.com>
2025-05-02 23:08:14 -0700

    Animate the outlining of the Workday logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from 'react';
 
 import './App.scss';
+import { WorkdayLogo } from './component/logo/Workday';
 
-const App = (): ReactElement => <div className='app'>My App</div>;
+const App = (): ReactElement => <div className='app'><WorkdayLogo /></div>;
 
 export default App;
